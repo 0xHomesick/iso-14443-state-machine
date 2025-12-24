b838b2e405030cb9d3ce07c91362126de3d1aba0
OxHomesick <hiscolorsdontrun@proton.me>
2025-12-25 00:17:23 +0300

    Updated opcode data types

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ class State (Enum):
     HALT:str = "Halt"
     
 class Opcode(Enum):
-    REQA:hex = 0x26
-    WUPA:hex = 0x52
-    HLTA:hex = 0X50 #parameter 0X00
-    ANTICOLLISION:hex = 0x20 #parameter 0x93, 0x95, 0x97 (cascade levels)
-    SELECT:hex = 0x70 #same params as anticollision
+    REQA:int = 0x26
+    WUPA:int = 0x52
+    HLTA:int = 0X50 #parameter 0X00
+    ANTICOLLISION:int = 0x20 #parameter 0x93, 0x95, 0x97 (cascade levels)
+    SELECT:int = 0x70 #same params as anticollision
     
 def select_state()->State:
     state = inquirer.list_input("Select state", choices=[state.name for state in State])
